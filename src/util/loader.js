470628c25810c94d8d4f3e231cbf541b770b9887
Konstantin Komelin <konstantin.komelin@gmail.com>
2021-10-01 01:23:25 +0300

    Extracted some code and settings from the main loader function to improve readability.

--- src/util/loader.js
+++ src/util/loader.js
@@ -1,61 +1,83 @@
-export function uploadcareLoader({ root, src, width, quality }) {
-  const isOnCdn = /^https?:\/\/ucarecdn\.com/.test(src)
+const MAX_OUTPUT_IMAGE_DIMENSION = 3000;
+const SKIPPED_EXTENSIONS = ["svg", "gif"];
 
-  if (process.env.NODE_ENV !== 'production') {
-    if (!isOnCdn && src.startsWith('/')) {
+function getExtension(filename) {
+  return filename.toLowerCase().split("?")[0].split("#")[0].split(".")[1];
+}
+
+function getFilename(url) {
+  return url.substring(1 + url.lastIndexOf("/"));
+}
+
+function trimTrailingSlash(url) {
+  return url.replace(/\/$/, "");
+}
+
+function convertToUploadcareQuality(requestedQuality) {
+  /**
+   * Uploadcare doesn't support integer-based quality modificators,
+   * so we need to map them onto uploadcare's equivalents
+   */
+  const names = ["lightest", "lighter", "normal", "better", "best"];
+  const intervals = [0, 38, 70, 80, 87, 100];
+  const nameIdx = intervals.findIndex((min, idx) => {
+    const max = intervals[idx + 1];
+    return min <= requestedQuality && requestedQuality <= max;
+  });
+  return names[nameIdx];
+}
+
+/**
+ * Get max resize width.
+ *
+ * Output image dimension is limited to 3000px,
+ * but it can be increased by explicitly setting /format/jpeg/ through API params.
+ */
+function getMaxResizeWidth(requestedWidth) {
+  return Math.min(Math.max(requestedWidth, 0), MAX_OUTPUT_IMAGE_DIMENSION);
+}
+
+export function uploadcareLoader({ root, src, width, quality }) {
+  const isOnCdn = /^https?:\/\/ucarecdn\.com/.test(src);
+
+  if (process.env.NODE_ENV !== "production") {
+    if (!isOnCdn && src.startsWith("/")) {
       throw new Error(
-        `Failed to parse "${src}" in "next/image", Uploadcare loader doesn't support relative images`
-      )
+        `Failed to parse "${src}" in "next/image", Uploadcare loader doesn't support relative images.`
+      );
     }
 
     if (!isOnCdn && !/^https?:\/\/.+\.ucr\.io\/?$/.test(root)) {
       throw new Error(
         `Failed to parse "${root}" in "next/image", Uploadcare loader expects proxy endpoint like "https://YOUR_PUBLIC_KEY.ucr.io".`
-      )
+      );
     }
   }
 
-  const filename = src.substring(1 + src.lastIndexOf('/'))
-  const extension = filename
-    .toLowerCase()
-    .split('?')[0]
-    .split('#')[0]
-    .split('.')[1]
+  const filename = getFilename(src);
+  const extension = getExtension(filename);
 
-  if (['svg', 'gif'].includes(extension)) {
-    return isOnCdn ? src : `${root.replace(/\/$/, '')}${src}`
+  // Some extensions are not processed by Uploadcare, e.g. SVG.
+  if (SKIPPED_EXTENSIONS.includes(extension)) {
+    return isOnCdn ? src : `${trimTrailingSlash(root)}${src}`;
   }
 
-  /**
-   * Output image dimensions is limited to 3000px
-   * It can be increased by explicitly setting /format/jpeg/
-   */
-  const maxResizeWidth = Math.min(Math.max(width, 0), 3000)
+  const maxResizeWidth = getMaxResizeWidth(width);
   // Demo: https://ucarecdn.com/a6f8abc8-f92e-460a-b7a1-c5cd70a18cdb/-/format/auto/-/resize/300x/vercel.png
-  const params = ['format/auto', 'stretch/off', `resize/${maxResizeWidth}x`]
+  const params = ["format/auto", "stretch/off", `resize/${maxResizeWidth}x`];
 
   if (quality) {
-    /**
-     * Uploadcare doesn't support integer-based quality modificators,
-     * so we need to map them onto uploadcare's equivalents
-     */
-    const names = ['lightest', 'lighter', 'normal', 'better', 'best']
-    const intervals = [0, 38, 70, 80, 87, 100]
-    const nameIdx = intervals.findIndex((min, idx) => {
-      const max = intervals[idx + 1]
-      return min <= quality && quality <= max
-    })
-    params.push(`quality/${names[nameIdx]}`)
+    params.push(`quality/${convertToUploadcareQuality(quality)}`);
   } else {
-    params.push('quality/smart')
+    params.push("quality/smart");
   }
 
-  const paramsString = '/-/' + params.join('/-/') + '/'
+  const paramsString = "/-/" + params.join("/-/") + "/";
 
   if (isOnCdn) {
-    const withoutFilename = src.slice(0, src.lastIndexOf('/'))
-    return `${withoutFilename}${paramsString}${filename}`
+    const withoutFilename = src.slice(0, src.lastIndexOf("/"));
+    return `${withoutFilename}${paramsString}${filename}`;
   }
 
-  return `${root.replace(/\/$/, '')}${paramsString}${src}`
+  return `${trimTrailingSlash(root)}${paramsString}${src}`;
 }
