eb56cf3ab85d8585d9ec2bd521fa3947f8d6c4dc
Konstantin Komelin <konstantin.komelin@gmail.com>
2021-10-03 11:04:01 +0300

    Enabled webpack production mode properly.

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,40 @@
 const path = require("path");
 
-const environment = JSON.stringify(process.env.NODE_ENV) || 'none';
+module.exports = (env) => {
+  const mode = env.production ? 'production' : 'development';
 
-module.exports = {
-  mode: environment,
-  entry: path.resolve(__dirname, './src/index.js'),
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index.js",
-    // library: {
-    //   name: "nextjsUploadcare",
-    //   type: "umd",
-    // },
-  },
-  externals: {
-    "next/image": "next/image"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          }
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-  },
+  return {
+    mode,
+    entry: path.resolve(__dirname, "./src/index.js"),
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      filename: "index.js",
+      // library: {
+      //   name: "nextjsUploadcare",
+      //   type: "umd",
+      // },
+    },
+    externals: {
+      "next/image": "next/image",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"],
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx"],
+    }
+  };
 };
