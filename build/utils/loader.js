e0c5420d4c9d6dd49cd3df5e11ba103f0d7ea1d1
Konstantin Komelin <konstantin.komelin@gmail.com>
2021-11-10 14:19:03 +0300

    chore: Rebuilt the demo project.

--- build/utils/loader.js
+++ build/utils/loader.js
@@ -14,30 +14,25 @@ function uploadcareLoader({ src, width, quality }) {
     const proxyEndpoint = customProxyEndpoint || (0, helpers_1.generateDefaultProxyEndpoint)(publicKey);
     const basePath = (0, helpers_1.trimTrailingSlash)(process.env.NEXT_PUBLIC_UPLOADCARE_APP_BASE_URL || '');
     const proxy = (0, helpers_1.trimTrailingSlash)(proxyEndpoint);
-    const isOnCdn = (0, helpers_1.isCdnUrl)(src, cdnDomain);
-    if (!(0, helpers_1.isProduction)() && !isOnCdn) {
+    const isProductionMode = (0, helpers_1.isProduction)();
+    const isImageOnCdn = (0, helpers_1.isCdnUrl)(src, cdnDomain);
+    const isImageRelative = src.startsWith('/');
+    // Development mode; not on CDN.
+    if (!isProductionMode && !isImageOnCdn) {
         const isPublicKeySet = !(0, helpers_1.isDotenvParamEmpty)(publicKey);
         const isCustomProxyEndpointSet = !(0, helpers_1.isDotenvParamEmpty)(customProxyEndpoint);
         if (!isPublicKeySet && !isCustomProxyEndpointSet) {
             throw new Error(`Both NEXT_PUBLIC_UPLOADCARE_PUBLIC_KEY and NEXT_PUBLIC_UPLOADCARE_CUSTOM_PROXY_DOMAIN are not set. Please set either one.`);
         }
-        if (src.startsWith('/')) {
+        if (isImageRelative) {
             return src;
         }
-    }
-    // Process local images in Production.
-    if ((0, helpers_1.isProduction)() && !isOnCdn && src.startsWith('/')) {
-        const isBasePathSet = !(0, helpers_1.isDotenvParamEmpty)(basePath);
-        if (!isBasePathSet) {
-            return src;
-        }
-        return `${basePath}${src}`;
     }
     const filename = (0, helpers_1.getFilename)(src);
     const extension = (0, helpers_1.getExtension)(filename);
     // Some extensions are not processed by Uploadcare, e.g. SVG.
     if (constants_1.NOT_PROCESSED_EXTENSIONS.includes(extension)) {
-        return isOnCdn ? src : `${basePath}${src}`;
+        return isImageOnCdn ? src : `${basePath}${src}`;
     }
     // Demo: https://ucarecdn.com/a6f8abc8-f92e-460a-b7a1-c5cd70a18cdb/-/format/auto/-/resize/300x/vercel.png
     const userParams = (0, helpers_1.parseUserParamsString)(userParamsString);
@@ -52,9 +47,18 @@ function uploadcareLoader({ src, width, quality }) {
     ]);
     const params = (0, helpers_1.mergeParams)(basicParams, userParams);
     const apiParamsString = '/-/' + params.join('/-/') + '/';
-    if (isOnCdn) {
+    if (isImageOnCdn) {
         const withoutFilename = src.slice(0, src.lastIndexOf('/'));
         return `${withoutFilename}${apiParamsString}${filename}`;
+    }
+    // Production mode; local image.
+    if (isProductionMode && isImageRelative) {
+        const isBasePathSet = !(0, helpers_1.isDotenvParamEmpty)(basePath);
+        // Return the relative url AS IS if the base path is not set.
+        if (!isBasePathSet) {
+            return src;
+        }
+        return `${proxy}${apiParamsString}${basePath}${src}`;
     }
     return `${proxy}${apiParamsString}${src}`;
 }
