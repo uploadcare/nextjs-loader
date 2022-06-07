3c678d1064bcde75c7c9c42046b83225e8131ced
nd0ut <nd0ut.me@gmail.com>
2022-06-07 15:56:23 +0300

    chore: refactor

--- src/components/UploadcareImage.tsx
+++ src/components/UploadcareImage.tsx
@@ -3,22 +3,29 @@ import React from 'react';
 import { getInt } from '../utils/helpers';
 import { uploadcareLoader } from '../utils/loader';
 
+const shouldOverrideBlurDataUrl = (props: ImageProps): boolean => {
+  return props.placeholder === 'blur' && !props.blurDataURL;
+};
+
+const generateBlurDataUrl = (
+  src: string,
+  width: ImageProps['width']
+): string => {
+  const imageWidth = getInt(width);
+  const blurImageWidth = imageWidth ? Math.ceil(imageWidth * 0.01) : 10;
+  return uploadcareLoader({
+    src: src,
+    width: blurImageWidth,
+    quality: 0
+  });
+};
+
 export function UploadcareImage({
   blurDataURL,
   ...props
 }: ImageProps): JSX.Element {
-  if (
-    props.placeholder === 'blur' &&
-    !blurDataURL &&
-    typeof props.src === 'string'
-  ) {
-    const imageWidth = getInt(props.width);
-    const blurImageWidth = imageWidth ? Math.ceil(imageWidth * 0.01) : 10;
-    blurDataURL = uploadcareLoader({
-      src: props.src,
-      width: blurImageWidth,
-      quality: 0
-    });
+  if (typeof props.src === 'string' && shouldOverrideBlurDataUrl(props)) {
+    blurDataURL = generateBlurDataUrl(props.src, props.width);
   }
   return (
     <Image loader={uploadcareLoader} blurDataURL={blurDataURL} {...props} />
