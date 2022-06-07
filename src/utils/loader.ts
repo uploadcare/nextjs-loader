980cba97bec060cd1332381359a240b68de02adc
nd0ut <nd0ut.me@gmail.com>
2022-06-08 02:17:47 +0300

    chore: add todo about StaticImport

--- src/utils/loader.ts
+++ src/utils/loader.ts
@@ -21,6 +21,7 @@ import {
   trimTrailingSlash
 } from './helpers';
 
+// TODO: add support for StaticImport, see https://nextjs.org/docs/basic-features/image-optimization#local-images
 export function uploadcareLoader({
   src,
   width,
