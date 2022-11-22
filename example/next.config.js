65446bc5a8fcf7ec638a3361e3844220e6a442ad
nd0ut <nd0ut.me@gmail.com>
2022-11-22 19:05:17 +0400

    chore: update demo next & react deps

--- example/next.config.js
+++ example/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    loader: 'custom',
+    loaderFile: "./node_modules/@uploadcare/nextjs-loader/build/loader.js",
+  },
 }
